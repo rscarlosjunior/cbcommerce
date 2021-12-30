62109c201f71098170c182e6e4165d35ccb23619
Carlos Junior - Wine <64548465+globalsysroberto@users.noreply.github.com>
2021-12-30 10:03:58 -0300

    [feat] include types

---
+++ src/types/IProduct.interface.tsx
@@ -0,0 +1,20 @@
+import ApiSearchResponse from "@prismicio/client/types/ApiSearchResponse";
+
+export interface IProduct {
+  data: object,
+  uid: number
+  url?: string
+  name: string
+  stars?: string
+  price: number
+  promotionPrice?: number
+  parcel?: number
+  splitedPrice?: number
+  available: boolean
+}
+
+export interface IProducts {
+  [x: string]: any;
+  products: IProduct[],
+  api?: ApiSearchResponse
+}
