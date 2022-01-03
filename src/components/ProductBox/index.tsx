302f9717d0e1cf123163ae7a6d0f997a008e8d23
Carlos Junior - Wine <75792848+rscarlosjunior@users.noreply.github.com>
2022-01-03 02:33:08 -0300

    chore - new data for product box

--- src/components/ProductBox/index.tsx
+++ src/components/ProductBox/index.tsx
@@ -12,41 +12,42 @@ import {
     Bbuton,
     Box
 } from './styles'
+import { RichText } from 'prismic-dom';
 
 export const ProductBox = ({
     ...item
 }: IProduct) => {
     return (
         <Pwrapper>
-            <img src={item.url} alt={item.name}/>
+            <img src={item.data.img.url} alt={item.data.name}/>
             <Dwrapper>
                 <Box>
                     <ProductName>
-                        {item.name}
+                        {RichText.asText(item.data.name)}
                     </ProductName>
-                    <Rating name="read-only" value={item.stars} readOnly />
+                    <Rating name="read-only" value={item.data.stars} readOnly />
                 </Box>
-                {item.promotionPrice ? (
+                {item.data.promotionPrice ? (
                     <>
                         <ProductSalePrice>
-                            de R${item.price}
+                            de R${item.data.price}
                         </ProductSalePrice>
                         <ProductPrice>
-                            por R${item.promotionPrice}
+                            por R${item.data.promotionPrice}
                         </ProductPrice>
                     </>
 
                 ):(
                     <ProductPrice>
-                        por R${item.price}
+                        por R${item.data.price}
                     </ProductPrice>
                 )}
-                {item.parcel && (
+                {item.data.parcel && (
                     <ProductParcelPrice>
-                        ou em {item.parcel}x de R${item.splitedPrice}
+                        ou em {item.data.parcel}x de R${item.data.splitedPrice}
                     </ProductParcelPrice>
                 )}
-                {item.available && (
+                {item.data.avaible && (
                     <Bbuton>Comprar</Bbuton>
                 )}
             </Dwrapper>
