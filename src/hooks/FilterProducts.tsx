1b35d0df88634f940c76db8d7eabe4397b6fcdab
Carlos Junior - Wine <75792848+rscarlosjunior@users.noreply.github.com>
2022-01-03 04:17:42 -0300

    fix hook

--- src/hooks/FilterProducts.tsx
+++ src/hooks/FilterProducts.tsx
@@ -1,12 +1,15 @@
+import { IProduct } from "@/types/IProduct.interface";
+import { RichText } from "prismic-dom";
+
 export const useFilter = () => {
-    const productFilter = (productList:[], query:string) => {
+    const productFilter = (productList:IProduct[], query:string) => {
         if (!query) {
             return productList;
         }
     
-        return productList.filter((productList: { name: string; }) => {
-            const postName = productList.name.toLowerCase();
-            return postName.includes(query);
+        return productList.filter((product) => {
+            const productName = RichText.asText(product.data.name).toLowerCase();
+            return productName.includes(query);
         });
     }
     return {
