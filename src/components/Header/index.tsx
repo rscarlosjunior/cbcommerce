ec6011919dff87ddb9e27b9f371f37f5092d8efe
Carlos Junior - Wine <75792848+rscarlosjunior@users.noreply.github.com>
2022-01-03 02:33:54 -0300

    feat - include breaking point on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { SearchBar } from '@/components/SearchBar'
-import { Hcontent } from './styles'
+import { Hcontent,IconSpacing,IconWrapper, MyAccount } from './styles'
+import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
+import PermIdentitySharpIcon from '@mui/icons-material/PermIdentitySharp';
+import { useMediaQuery } from '@mui/material';
+import { Menu } from '@mui/icons-material';
 
 interface HeaderProps {
     handleProductFilter?: (v?:string) => void
@@ -11,20 +15,43 @@ export const Header:React.FC<HeaderProps> = ({
     const { search } = window.location; 
     const query = new URLSearchParams(search).get('s');
     const [searchQuery, setSearchQuery] = useState(query || '');
+    const xs = useMediaQuery('(max-width:797px)');
 
     const handleFilter = (value:string) => {
-        setSearchQuery(value)
         if (typeof handleProductFilter !== 'function') return
+        setSearchQuery(value)
         handleProductFilter(searchQuery)
     }
 
     return (
-        <Hcontent>
-            <img src="https://i.imgur.com/R2kFhZw.png" alt=""/>
-            <SearchBar
-                searchQuery={searchQuery}
-                setSearchQuery={handleFilter}
-            />
-        </Hcontent>
+        <>
+            <Hcontent>
+                {xs && (<Menu sx={{ fontSize: 40 }}/>)}
+                <img src="https://i.imgur.com/R2kFhZw.png" alt=""/>
+                {!xs && (
+                    <SearchBar
+                        searchQuery={searchQuery}
+                        setSearchQuery={handleFilter}
+                    />
+                )}
+                <IconWrapper>
+                    {!xs && (
+                        <IconSpacing>
+                            <PermIdentitySharpIcon sx={{ fontSize: 40 }} />
+                            <MyAccount>Minha Conta</MyAccount>
+                        </IconSpacing>
+                    )}
+                    <IconSpacing>
+                        <ShoppingCartSharpIcon  sx={{ fontSize: 35 }} />
+                    </IconSpacing>
+                </IconWrapper>
+            </Hcontent>
+            {xs && (
+                <SearchBar
+                    searchQuery={searchQuery}
+                    setSearchQuery={handleFilter}
+                />
+            )}
+        </>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,7 +2,26 @@ import styled from 'styled-components'
 
 export const Hcontent = styled.div`
     flex-direction: row;
+    margin: 10px 0 10px 0;
     display:flex;
     align-items:center;
-    justify-content: space-between;
+    justify-content: space-around;
+`
+export const IconWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    @media screen and (min-width: 798px) {
+        width:100%;
+    }
+    max-width: 185px;
+`
+export const IconSpacing = styled.div`
+    margin: 0px 5px 0px 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+export const MyAccount = styled.span`
+    font-size:15px;
+    color: #7A7A7A;
 `
