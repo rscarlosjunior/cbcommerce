9b84d969f913f87d3f779f46c4222336795dbbfc
Carlos Junior - Wine <75792848+rscarlosjunior@users.noreply.github.com>
2022-01-04 08:34:13 -0300

    include localstorage hook

---
+++ src/hooks/LocalStorage.tsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from "react"
+
+function useLocalStorageLite<T>(key: string) {
+    const [state, setState] = useState<T | null>(() => {
+        const exValue = localStorage.getItem(key)
+        if (exValue) {
+            return JSON.parse(exValue) as T
+        }
+        return null
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(state))
+    }, [state])
+
+    const storageWatcher = useCallback(
+        (e: StorageEvent) => {
+            if (e.newValue) {
+                setState((currState) => {
+                    const newDat = JSON.parse(e.newValue || "null")
+                    return newDat == state ? newDat : currState
+                })
+            }
+        },
+        [state]
+    )
+
+    useEffect(() => {
+        window.addEventListener("storage", storageWatcher)
+        return () => {
+            window.removeEventListener("storage", storageWatcher)
+        }
+    }, [state])
+
+    return { state, setState }
+}
+
+export default useLocalStorageLite;
